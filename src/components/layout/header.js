07f5d9e0242e8088fe19272d74f3d8588941751f
Ivan Ip <m@lifeho.me>
2020-11-06 17:41:59 +0800

    update header nav with slight flavoured official styling

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -24,36 +24,54 @@ const TelegramHeaderNav = styled(Navbar)`
     }
   }
 
-  .navitem-active{
-    padding-bottom: .1rem;
-    box-shadow: inset 0 -3px 0 0 #179cde;
+  .dropdown-toggle::after{
+    border: none; // important override to bootstrap styling
   }
 
-  .nav-item a.nav-link, .nav-item.show a.nav-link, .nav-item a.nav-link:focus {
+  .nav-item > a.nav-link,
+  .nav-item.show > a.nav-link,
+  .nav-item > a.nav-link:focus,
+  .nav-item > a.nav-link:hover {
     color: #08c;
     padding: .89rem;
-    box-shadow: none;
-    transition:
-      box-shadow .34s cubic-bezier(.165, .84, .44, 1),
-      color .34s cubic-bezier(.165, .84, .44, 1);
+    position: relative;
   }
 
-  .nav-item a.nav-link:hover {
-    color: #1ab3ff;
-    box-shadow: inset 0 -3px 0 0 #179cde;
+  .nav-item > a:after{
+    display: block;
+    height: 3px;
+    background: #179cde;
+    position: absolute;
+    border-radius: 2px 2px 0 0;
+    content: "";
+    left: 4px;
+    right: 4px;
+    opacity: 0;
+    bottom: 0;
     transition:
-      box-shadow .34s cubic-bezier(.165, .84, .44, 1),
-      color .34s cubic-bezier(.165, .84, .44, 1);
+      opacity .2s cubic-bezier(.165, .84, .44, 1),
+      transform .2s cubic-bezier(.165, .84, .44, 1);
+    transform-origin: bottom;;
+    transform: scaleX(0.3) scaleY(0);
+  }
+
+  .nav-item > a.navitem-active:after,
+  .nav-item.show > a:after,
+  .nav-item > a:hover:after {
+    opacity: 1;
+    transform: scaleX(1.0) scaleY(1.0);
   }
 `
 
 const Header = ({ t, i18n }) => (
   <TelegramHeaderNav expand="lg" sticky="top">
     <Container>
-      <Navbar.Brand as={Link} to="/">TelegramHK</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse>
         <Nav>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/" activeClassName="navitem-active">{t('navbar.index')}</Nav.Link>
+          </Nav.Item>
           <Nav.Item>
             <Nav.Link as={Link} to="/apps" activeClassName="navitem-active">{t('navbar.apps')}</Nav.Link>
           </Nav.Item>
@@ -65,7 +83,7 @@ const Header = ({ t, i18n }) => (
           </Nav.Item>
         </Nav>
         <Nav className="ml-auto">
-          <NavDropdown title={t('i18n.selector.label')} id="langselector">
+          <NavDropdown title={t('i18n.selector.label')}>
             <NavDropdown.Item onClick={()=>i18n.changeLanguage("en")}>English</NavDropdown.Item>
             <NavDropdown.Item onClick={()=>i18n.changeLanguage("hk")}>繁體中文 (香港)</NavDropdown.Item>
           </NavDropdown>
