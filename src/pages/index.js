9eb7bc4359ae20a8f41e57e6200b41480ad2bd14
Ivan Ip <m@lifeho.me>
2020-11-06 18:39:36 +0800

    Initial commit for landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,36 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout/master"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+// i18n import
+import { withTrans } from '../i18n/withTrans'
+
+import { Container, Row, Col } from 'react-bootstrap'
+
+const MainContainer = styled(Container)`
+  &&&{
+    margin-top: 34px;
+  }
+`
+
+const IndexPage = ({ t }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <MainContainer>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <Link to="/">
+            <img src="/images/official-assets/telegram_official_logo.png" height={128} alt="" style={{display: "block"}} className="mx-auto" />
+            <div className="tl_main_logo_title_image" alt="Telegram"></div>
+          </Link>
+          <p className="tl_main_logo_lead">{t("landingpage.slogan")}</p>
+        </Col>
+      </Row>
+    </MainContainer>
   </Layout>
 )
 
-export default IndexPage
+export default withTrans(IndexPage)
